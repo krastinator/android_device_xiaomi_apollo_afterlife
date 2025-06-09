eb3e7dd980e64a3d69c19a64f57afb91681a3a2f
procrastinator-dev <murtazakolachi10@gmail.com>
2025-06-10 02:55:23 +0500

    Afterlife bringup

--- afterlife_apollo.mk
+++ afterlife_apollo.mk
@@ -14,7 +14,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-PRODUCT_NAME := lineage_apollo
+PRODUCT_NAME := afterlife_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -25,6 +25,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="apollo_global-user 12 RKQ1.211001.001 V14.0.4.0.SJDMIXM release-keys" \
     BuildFingerprint=Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys
-
-# Private
-include vendor/sign/keys/keys.mk
