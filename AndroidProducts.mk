8f4167af4adebb3b5980ea10f3c8ff83bf92ddaa
procrastinator-dev <murtazakolachi10@gmail.com>
2025-06-10 02:56:20 +0500

    Afterlife bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollo.mk
+    $(LOCAL_DIR)/afterlife_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollo-user \
-    lineage_apollo-userdebug \
-    lineage_apollo-eng
+    afterlife_apollo-user \
+    afterlife_apollo-userdebug \
+    afterlife_apollo-eng
